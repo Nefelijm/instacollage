08fe3d2f9f0e76aeaaafd7e6e739f19d73b41922
patty8909 <patty90_30@hotmail.com>
2018-02-07 19:58:35 -0500

    añade avance funcionalidad y html

--- js/app.js
+++ js/app.js
@@ -1,5 +1,27 @@
-window.addEventListener('load', function() {
+window.addEventListener('load', () => {
+// se agrega el evento dragstart a los elementos arrastrables, y se especifican los datos de arrastre.
 
+// const imgDrag = document.getElementsByClassName('styleImgDrag');
+// const imgDrop = document.getElementsByClassName('imgDrop');
 
+  document.addEventListener('dragstart', (ev) => {
+    ev.dataTransfer.setData('text', ev.target.className);
+    console.log('its dragging...');
+  });
 
+  document.addEventListener('dragover', (ev) => {
+    ev.preventDefault();
+    console.log('its dragover...');
+  });
+
+  document.addEventListener('drop', (ev) => {
+  // let dropItem = ev.dataTransfer.getData('text');
+    ev.preventDefault();
+    if (event.target.className === 'imgDrop') {
+      let imagen = ev.dataTransfer.getData('text');
+      event.target.appendChild(document.getElementById(imagen));
+    }
+  });
+
+// hace falta comunicar el evento dragstart con el evento drop
 });
